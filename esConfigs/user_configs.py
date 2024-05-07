b5ac69889b88124be04de1ae4bd4e4fba2a697f5
yoominlee <yoomin0104@gmail.com>
2024-05-07 20:36:20 +0900

    add linked es codes

---
+++ esConfigs/user_configs.py
@@ -0,0 +1,9 @@
+# Need to update
+
+IMAGENET_ES_ROOT_DIR = "/path/to/ImageNet-ES-ROOT-DIR/"
+SWIN_PT = "/path/to/Swin-weight-file"
+RESNET18_PT = "/home/ymlee/git/ImageNet-ES/models/resnet18.pth"
+EN_PT = "/path/to/EfficientNet-weight-file"
+VIT_PT = "/path/to/vit-weight-file"
+
+TARGET_OOD_POSTPROCESSORS = ['msp', 'odin', 'react', 'vim', 'ash']
